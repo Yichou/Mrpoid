266afc0dcb6735caefed36b1f003ac84e60e6ea7
Yichou <yichouangle@gmail.com>
2013-08-31 21:12:35 +0800

    logo Signed-off-by: Yichou <yichouangle@gmail.com>

--- src/com/mrpoid/mrplist/WelcomeActivity.java
+++ src/com/mrpoid/mrplist/WelcomeActivity.java
@@ -2,6 +2,7 @@ package com.mrpoid.mrplist;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -11,6 +12,15 @@ public class WelcomeActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		SharedPreferences sp = getPreferences(0);
+		if(!sp.getBoolean("showLogo", true)) {
+			startActivity(new Intent(WelcomeActivity.this, MrplistActivity.class));
+			finish();
+			return;
+		}
+		
+		sp.edit().putBoolean("showLogo", false).commit();
 		
 		setContentView(R.layout.activity_welcome);
 		
